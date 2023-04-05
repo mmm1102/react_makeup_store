2b988261fd625b47b5a0348234f37706d7a94685
Marija <marijajovicmusic@gmail.com>
2023-04-05 11:14:37 +0200

    contact us, FAQs

--- front/src/components/Footer/ContactUs.js
+++ front/src/components/Footer/ContactUs.js
@@ -3,6 +3,8 @@ import React from 'react'
 const ContactUs = () => {
   return (
     <div>
+      <h3 style={{marginTop:"3rem"}}>Contact Us</h3>
+      <div style={{width:"50vw", margin:"0 auto", marginTop:"5rem"}}><p>For questions about your order, please contact us at 1-800-123-4567</p></div>
       
     </div>
   )
--- front/src/components/Footer/FAQs.js
+++ front/src/components/Footer/FAQs.js
@@ -3,7 +3,16 @@ import React from 'react'
 const FAQs = () => {
   return (
     <div>
-      
+      <h3 style={{marginTop:"3rem"}}>Frequently Asked Questions</h3>
+
+      <h4 style={{marginTop:"3rem"}}>Shipping Costs & Delivery Times</h4>
+      <div style={{width:"50vw", margin:"0 auto", marginTop:"3rem"}}>Makeup Store offers 1-3 business day shipping as our standard shipping option on all merchandise orders.  For merchandise orders totaling less than $50, there is a shipping cost of $6.95 for standard shipping. Depending on the carrier, your order may arrive on a weekend.</div>
+
+      <h4 style={{marginTop:"2rem"}}>Finding Products</h4>
+      <div style={{width:"50vw", margin:"0 auto", marginTop:"2rem"}}><p>Know exactly what you want? Or perhaps you're just searching for a matte lipstick? Use our comprehensive Search box located in the upper left corner of our site to find the exact product you need, or browse through the results that fit your criteria. </p></div>
+
+      <h4 style={{marginTop:"2rem"}}>Returns Policy</h4>
+      <div style={{width:"50vw", margin:"0 auto", marginTop:"2rem"}}><p>If you are not completely satisfied with Makeup Store purchase or gift for any reason, Makeup Store welcomes you to return new or gently used products for a full refund to your original method of payment if returned within 30 days of purchase, in most cases. In order to complete the return, you must have a proof of purchase.</p></div>
     </div>
   )
 }
